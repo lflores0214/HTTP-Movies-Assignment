19a507d0f9b8722b1656b0ec81422a000adc17f5
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-11-14 15:06:20 -0700

    added dependencies, created new branch, and started on UpdateForm component

---
+++ client/src/Movies/UpdateForm.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+const initialMovie = {
+id: "",
+title: "",
+director: "",
+metascore: "",
+stars: [""],
+}
+
+const UpdateForm = props => {
+   
+
+    
+}
+
+export default UpdateForm
